cdcc66b302665d1f8e802e943ed52bb56543d191
Andrey Repin <anrdaemon@yandex.ru>
2026-05-15 01:45:32 +0300

    Added TaskStatus entity

---
+++ src/main/java/hexlet/code/mapper/TaskStatusMapper.java
@@ -0,0 +1,26 @@
+package hexlet.code.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+import hexlet.code.dto.task.TaskStatusCreateDTO;
+import hexlet.code.dto.task.TaskStatusDTO;
+import hexlet.code.dto.task.TaskStatusUpdateDTO;
+import hexlet.code.model.TaskStatus;
+
+@Mapper(uses = {
+        JsonNullableMapper.class }, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+
+public abstract class TaskStatusMapper {
+
+    public abstract TaskStatus map(TaskStatusDTO dto);
+
+    public abstract TaskStatusDTO map(TaskStatus taskStatus);
+
+    public abstract TaskStatus map(TaskStatusCreateDTO dto);
+
+    public abstract void update(TaskStatusUpdateDTO taskStatusUpdateDTO, @MappingTarget TaskStatus taskStatus);
+}
