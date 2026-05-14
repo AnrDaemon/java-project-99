aa6f2a507cde68d4f398d7d2708162d835ac381a
Andrey Repin <anrdaemon@yandex.ru>
2026-05-15 02:07:09 +0300

    Added tasks workflow

---
+++ src/main/java/hexlet/code/dto/task/TaskCreateDTO.java
@@ -0,0 +1,33 @@
+package hexlet.code.dto.task;
+
+import java.util.Set;
+
+import org.openapitools.jackson.nullable.JsonNullable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskCreateDTO {
+
+    private Long index;
+
+    @NotNull
+    @NotBlank
+    private String title;
+
+    private String content;
+
+    @NotNull
+    private String status;
+
+    @JsonProperty("assignee_id")
+    private JsonNullable<Long> assigneeId;
+
+    private Set<Long> taskLabelIds;
+}
